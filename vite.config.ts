321ea210d9d3f16501df7549710b5aebd46c3ab5
Gavin <gsstockwell@gmail.com>
2025-02-09 13:30:16 -0800

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-  },
   server: {
     port: 3000,
   },
